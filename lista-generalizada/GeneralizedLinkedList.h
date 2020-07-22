7925bdf905e87212e9960ba3963bc171a4de2e50
joaooliveira10 <joaoangello10@gmail.com>
2020-07-22 19:59:03 -0400

    Adicionado pasta para desenvolvimento da Lista Generalizada

---
+++ lista-generalizada/GeneralizedLinkedList.h
@@ -0,0 +1,22 @@
+#ifndef DataStructure_GeneralizedLinkedList_h
+#define DataStructure_GeneralizedLinkedList_h
+enum boolean {
+    true = 1, false = 0
+};
+typedef enum boolean bool;
+typedef struct Node {
+    int type; //0 atomo e 1 lista
+    union {
+        int atom;
+        struct Node *list;
+    }atomList;
+    struct Node *next;
+}Node;
+int addAtom(Node **list, int atom);
+int addList(Node **list, Node **subList);
+Node* head(Node *list);
+Node* tail(Node *list);
+void show(Node *list);
+boolean search(Node *list, int atom);
+int depth(Node *list);
+#endif
