10710fa20b19d41a7d54df45ebb5649731fdd5b9
Alhambra98 <thaysgss@hotmail.com>
2020-07-27 21:25:40 -0400

    Atividade AddAtom atualizada

--- lista-generalizada/GeneralizedLinkedList.c
+++ lista-generalizada/GeneralizedLinkedList.c
@@ -1,3 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "generallist.h"
+
+//Grupo 1 - Thays e Victor
+// adiciona um átomo na lista
+int addAtom(Node **list, int atom) {
+    Node* node = (Node *) malloc(sizeof(Node));
+
+    node->type = 0;
+    node->atomList.atom = atom;
+    node->next = NULL;
+
+    // se o ponteiro que aponta para a lista for nulo
+    if (*list == NULL) {
+        *list = node;
+        return EXIT_SUCCESS;
+    }
+    //Ponteiro last recebe o ponteiro list com uma variável do tipo nó
+    Node *last = *list;
+    //Enquanto last aponta para próximo que é diferente de nulo, a variável last recebe ela mesma apontando para o próximo
+    while (last->next != NULL) {
+        last = last->next;
+    }
+    // Last aponta para next que recebe o nó
+    last->next = node;
+    return EXIT_SUCCESS;
+}
+
 int addList(Node **list, Node **subList){
 
     Node *newnode =(Node*) malloc(sizeof(Node));
