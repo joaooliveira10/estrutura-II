82a88c310a27f3a3110ade8537c985b7c3dc4645
Caroline Souza <60698148+CarolineSouza626@users.noreply.github.com>
2020-07-30 21:58:41 -0300

    Update hash.c

--- hash/hash.c
+++ hash/hash.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "Hash.h"
 
+void initHash(HashStruct *hashStruct){
+}
+bool isHashEmpty(HashStruct *hashStruct){
+}
+int hash(char *key){
+}
+int put(HashStruct *hashStruct, char *key, void *data, compare equal){
+}
+bool containsKey(HashStruct *hashStruct, char *key, compare equal){
+}
+void* get(HashStruct *hashStruct, char *key, compare equal){
+}
+void* removeKey(HashStruct *hashStruct, char *key, compare equal){
+}
+void showHashStruct(HashStruct *hashStruct){
+}
