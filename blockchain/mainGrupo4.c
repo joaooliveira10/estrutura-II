d5c2f1f520b668a20d2ad822ef2512f30fb616a2
joaooliveira10 <joaoangello10@gmail.com>
2020-08-12 00:01:07 -0400

    bool isValidNewBlock

---
+++ blockchain/mainGrupo4.c
@@ -0,0 +1,24 @@
+// bool isValidNewBlock Grupo 4 - João A. & Wigor 
+#include "Blockchain.h"
+
+int main() {
+    Blockchain blockchain;
+    printf("");
+    initBlockchain(&blockchain);
+
+    printf("%s\n",blockchain.genesisBlock->hash);
+    printf("%lu\n",blockchain.genesisBlock->timestamp);
+    
+    Block *newBlock2 = generateNextBlock(&blockchain,80.0);
+    addBlock(&blockchain, newBlock2);
+    
+    Block *newBlock3 = generateNextBlock(&blockchain,250.0);
+    addBlock(&blockchain, newBlock3);
+    
+    printf("Block 2 is valid? %i\n",isValidNewBlock(newBlock2, blockchain.genesisBlock));
+    newBlock2->data = 74.00;
+    printf("Blockchain is valid? %i\n",isBlockchainValid(&blockchain));
+
+    printf("size: %u\n",blockchain.latestBlock->index);
+    return 0;
+}
